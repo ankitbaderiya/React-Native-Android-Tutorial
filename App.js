17c670cc216fd79824a89d5e73a8c7b9b061eb11
Ankit Baderiya <ankitbaderiya@gmail.com>
2017-12-12 17:41:38 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { StackNavigator, } from 'react-navigation';
+import SearchPage from './SearchPage';
+
+const App = StackNavigator({
+  Home: { screen: SearchPage }
+});
+
+export default App;
